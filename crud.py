9f1f15c2d69637dfb86cf5efc2cfbbcd84e683f4
Luisgsm324 <luisf.moreira324@gmail.com>
2025-03-16 23:32:55 -0300

    fix: correções gerais no codigo

--- crud.py
+++ crud.py
@@ -2,7 +2,7 @@ import pymongo
 import structures
 
 class CrudDB:    
-    def __init__(self, local_condition, db_name, collection_name, primary_key="cpf", url=""):
+    def __init__(self, local_condition, db_name, collection_name, primary_key, url=""):
         if local_condition is True:
             self.client = pymongo.MongoClient("localhost", 27017)
         else:
@@ -16,21 +16,10 @@ class CrudDB:
 
         if self.collection is not None:
             if self._verify_key(primary_key): # A inserção de nenhum valor vai cair aqui por default
-                self.collection.create_index(primary_key)
+                self.collection.create_index(primary_key, unique=True)
             else:
-                self.collection.create_index(default_key)   
-
-    # Passar esse método para a outra classe depois
-    def _format_content(self, content, divisor=True):
-        format_keys = structures.DOCUMENT
-        for key in content.keys():
-            old_key = key
-            if key in format_keys.keys():
-                key = format_keys[key]
-            print(f"{key}: {content[old_key]}")
-        if divisor is True:    
-            print("------------------------")      
-
+                self.collection.create_index(default_key, unique=True)   
+    
     # Verificar presença de chave no "structures.py"
     def _verify_key(self, key):
         if key in structures.DOCUMENT:
@@ -50,11 +39,7 @@ class CrudDB:
         #    print("Resultado da seleção sem filtros:")
        # else:
         #    print(f"Resultado da seleção com o filtro {query}:")
-
-        #for x in content:
-        #    self._format_content(x)
-                      
-
+                
     # Atualizar para adaptar ao parameter primary_key
     def find_by_primary_key(self, key):
         # Return values:
@@ -68,19 +53,16 @@ class CrudDB:
     def create_customer(self, primary_key, values=structures.DOCUMENT):
         # False --> Não foi possível executar a criação
         # True  --> Criação feita com sucesso        
-        has_already_register, content = self.find_by_primary_key(primary_key)
-        if has_already_register == False:
-            document = structures.DOCUMENT
-            for key in document.keys():
-                if key in values.keys():
-                    document[key] = values[key]
+        document = structures.DOCUMENT
+        for key in document.keys():
+            if key in values.keys():
+                document[key] = values[key]
 
-            try:
-                self.collection.insert_one(document)
-                return True
-            except:
-                return False
-        return False        
+        try:
+            self.collection.insert_one(document)
+            return True
+        except:
+            return False       
 
 
     def update_customer(self, primary_key, values):
@@ -116,14 +98,18 @@ class CrudDB:
                 return False
         return False
 
-#system = CrudDB(local_condition=True, db_name='Clientes', collection_name='Clientes', primary_key="cpf")
+#system = CrudDB(local_condition=True, db_name='Clientes', collection_name='Clientes', primary_key="nome")
 
 #system.read_data()
 
-#condition = system.create_customer("123456789", "Alberto", 17, "alberto@gmail.com")
+#values = {
+#    "cpf": "44444444",
+#    "nome": "Lucas",
+#    "idade": 17,
+#    "email": "alberto@gmail.com"
+#}
+#condition = system.create_customer("Lucas", values)
 #print(condition)
 
-#system.find_by_cpf("99999999999")
-
-#condition = system.delete_customer_cpf("99999999999")
+#condition = system.delete_customer("Alberto")
 #print(condition)
--- system.py
+++ system.py
@@ -1,8 +1,10 @@
 from crud import CrudDB
 import os
+import time
+import structures
 
 class System:
-    def __init__(self, local_condition, db_name, collection_name, primary_key="", url=""):
+    def __init__(self, local_condition, db_name, collection_name, primary_key="cpf", url=""):
         self.crud = CrudDB(local_condition, db_name, collection_name, primary_key, url)
 
         self.show_options()
@@ -18,8 +20,8 @@ Escolha uma das opções abaixo:
 4) Deletar um cadastro de cliente
 -----------------------------"""
         print(text)
-        answer = input("")
-
+        answer = input()
+                
         match answer:
             case "1":
                 self.create_customer()
@@ -30,12 +32,91 @@ Escolha uma das opções abaixo:
             case "4":
                 self.delete_customer()
 
+    def _format_content(self, content, divisor=True):
+        format_keys = structures.DOCUMENT
+        for key in content.keys():
+            old_key = key
+            if key in format_keys.keys():
+                key = format_keys[key]
+            print(f"{key}: {content[old_key]}")
+        if divisor is True:    
+            print("------------------------")  
+
+    def _fill_document(self, read_statment=False):
+        document = structures.DOCUMENT
+        keys = structures.DOCUMENT.keys()
+        for key in keys:
+            while True:
+                input_text = document[key]
+                if key == self.crud.primary_key:
+                    input_text += " (obrigatório): "
+                else: 
+                    input_text += ": "
+                
+                answer = input(input_text)
+
+                if read_statment:
+                    break
+
+                if key == self.crud.primary_key:
+                    if answer == "":
+                        print("É necessário incluir um valor para esse campo.")
+                        continue
+                    else:
+                        break
+                else: 
+                    break
+            if read_statment:
+                if answer == "":
+                    document.pop(key, None)
+            else:    
+                document[key] = answer 
+        return document
 
     def create_customer(self):
-        pass
+        os.system('clear')
+        time.sleep(1)
+        text = """-----------------------------
+Preencha as informações:
+-----------------------------        
+"""
+        print(text)
+
+        # Preencher o documento 
+        document = self._fill_document()
+
+        condition = self.crud.create_customer(document[self.crud.primary_key], document)
+        if condition:
+            print(f"Cliente {document['nome']} criado com sucesso!")
+        else:
+            print("Não foi possível concluir a criação de cliente")
 
     def read_register(self):
-        pass
+        os.system('clear')
+        time.sleep(1)
+        text = """-----------------------------
+De qual forma você deseja realizar a leitura?
+-----------------------------
+1) Ler todos 
+2) Filtrar por chave        
+"""
+        print(text)
+        question = input()
+
+        if question == '1':
+            status, content = self.crud.read_data()
+            
+        else:
+            document = self._fill_document(read_statment=True)
+            status, content = self.crud.read_data(query=document)
+
+        if status and content != {}:
+            print("Registros: ")
+        else:
+            print("Não foi possível encontrar registros.")  
+
+        for x in content:
+            self._format_content(x)      
 
     def update_customer(self):
         pass
